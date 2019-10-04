b908053e5e000c1891591e547eeb9381f1db6144
aokisok <sevenclickok@gmail.com>
2019-10-04 22:38:24 +0100

    ensure votes have switched to using refTo only

--- src/post/vote.ts
+++ src/post/vote.ts
@@ -5,6 +5,7 @@ import { getAppVersion } from '../lib/schema-version';
 import { generateDateTags } from '../schema/date-tags';
 import { addStandardTags } from '../lib/schema-utils';
 import { copyRefTagsAppend } from '../schema/ref-to-tags';
+import { copyPathTags } from '../schema/path-tags';
 
 const VOTE_COST = '0.1';
 
@@ -26,7 +27,17 @@ export async function voteOnPost(wallet: any, post: PostTreeNode, upVote: boolea
   
   const postTags = post.post.tags;
   
-  copyRefTagsAppend(postTags, tags, post.id);
+  // Make sure we reference the original post, so our refTo 
+  // tree is correct. getOriginalNode() will always return the 
+  // original node of a post, regardless of whether its an edit 
+  // or not.
+  copyRefTagsAppend(postTags, tags, post.getOriginalNode().id);
+  copyPathTags(postTags, tags);
+  
+  // Record that we actually voted on an edit, for informational purposes.
+  if (post.isEdit) {
+    tags.wasToPe = post.id; 
+  }
   
   // Prepare and post TX 
   const target = (upVote && post.post.from) || undefined; 
@@ -56,10 +67,9 @@ export async function voteOnPost(wallet: any, post: PostTreeNode, upVote: boolea
   
   addStandardTags(tags as any);
   // assign last_tx to anchor to we can queue multiple posts.
-  //if (upVote) {
-    ;(tx as any).last_tx = anchor; 
-  //}
-
+  
+  ;(tx as any).last_tx = anchor; 
+  
   await arweave.transactions.sign(tx, wallet);
 
   const resp = await arweave.transactions.post(tx);
